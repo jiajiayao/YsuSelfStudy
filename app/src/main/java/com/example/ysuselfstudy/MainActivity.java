5c588b3a1a569428b6a20454d2db119ac5646cbe
Ahyer <zuixianlingfu@gmail.com>
2019-03-12 10:59:28 +0800

    实现了左边栏 DrawerLayout

--- app/src/main/java/com/example/ysuselfstudy/MainActivity.java
+++ app/src/main/java/com/example/ysuselfstudy/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.ysuselfstudy;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -25,6 +29,7 @@ import yuan.data.SchoolBuilding;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static ImageView image;
+    private DrawerLayout mDrawerLayout;
     private static TextView TimeView;
     TextView Today;
     String temp;
@@ -35,8 +40,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        }
         //每日必应图片
         image=(ImageView) findViewById(R.id.image);
         loadImage(image);
@@ -136,6 +150,18 @@ public class MainActivity extends AppCompatActivity {
         bg.add(new EmptyRoom("第7教学楼",16,"东"));
         childlist.add(bg);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home :
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+                default:
+        }
+        return  true;
     }
 
     public  static  void SetTime(String name)

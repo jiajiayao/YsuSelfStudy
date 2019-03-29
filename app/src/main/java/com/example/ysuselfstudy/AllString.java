3a40cbf40c8cf13ce2ac48884d180db83801a264
Ahyer <zuixianlingfu@gmail.com>
2019-03-29 22:03:29 +0800

    修复 Bug

--- app/src/main/java/com/example/ysuselfstudy/AllString.java
+++ app/src/main/java/com/example/ysuselfstudy/AllString.java
@@ -6,7 +6,6 @@ import android.text.StaticLayout;
 public class AllString {
     public  static  final String BiYing="http://guolin.tech/api/bing_pic";
     public  static  final String YSU="http://39.96.163.218:8080/SelfStudy/TransRoom";
-    public  static  String Today="今天也是充满希望的一天";
     public  static  final String APPID = "101560830";//QQ 登录相关的关键码
     public  static  final String YsuOffice="http://202.206.243.5/default2.aspx";//教务系统的登录界面
     public  static  final String YsuYanZhengMa="http://202.206.243.5/CheckCode.aspx";//教务系统的验证码
